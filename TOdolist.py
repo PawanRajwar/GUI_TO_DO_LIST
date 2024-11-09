a4f2f73b57f450be25c6a72aeaa1d96cd52e631d
Pawan Singh Rajwar <pawansinghrajwar@gmail.com>
2024-11-09 22:26:25 +0530

    Function for load tasks from file on startup

--- TOdolist.py
+++ TOdolist.py
@@ -11,3 +11,13 @@ root.config(bg="#1c1c1e")
 
 # Global list to store tasks
 tasks = []
+
+# Load tasks from file on startup
+def load_tasks():
+    global tasks
+    try:
+        with open("tasks.json", "r") as f:
+            tasks = json.load(f)
+            update_listbox()
+    except (FileNotFoundError, json.JSONDecodeError):
+        tasks = []
